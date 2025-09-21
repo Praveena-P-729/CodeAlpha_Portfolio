e9d7362cd553643b74691732339b61b172619692
Praveena-P-729 <praveena72939@gmail.com>
2025-09-21 22:11:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,4 @@ function scrollToSection(id) {
   document.getElementById(id).scrollIntoView({ behavior: "smooth" });
 }
 
-// Form submit interaction
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("Thank you! Your message has been sent.");
-});
+
